2b02c4dc28d14fbb20f997d04dfed3f0ff54876f
RohanVijayan <89291833+Ronthehero@users.noreply.github.com>
2026-03-20 18:57:52 +0530

    Create favicon.js

---
+++ JS/favicon.js
@@ -0,0 +1,4 @@
+  const _fav = document.getElementById("favicon");
+  const _frames = ["favicons/favicon-on.png", "favicons/favicon-off.png"];
+  let _fi = 0;
+  setInterval(() => { _fi = (_fi + 1) % 2; _fav.href = _frames[_fi]; }, 530);
